94059e0306ee81e10c1978bc2087d8af9346dd6b
Mike Lauer <mike@lauercloud.de>
2023-06-01 12:18:13 +0200

    adjust connection rules

--- src/lib/diagram/providers/RuleProvider.ts
+++ src/lib/diagram/providers/RuleProvider.ts
@@ -21,27 +21,29 @@ CustomRuleProvider.$inject = ["eventBus"];
 inherits(CustomRuleProvider, RuleProvider);
 
 
-CustomRuleProvider.prototype.init = function () {
-  this.addRule("shape.create", function (context: any) {
+CustomRuleProvider.prototype.init = function() {
+  this.addRule("shape.create", function(context: any) {
     const target = context.target,
       shape = context.shape;
 
     return target.parent === shape.target;
   });
 
-  this.addRule("connection.create", function (context: any) {
+  this.addRule("connection.create", function(context: any) {
     const source = context.source,
       target = context.target;
     if (source.businessObject && target.businessObject) {
-      if(source.businessObject.type == ObjectType.Gropius && target.businessObject.type == ObjectType.Gropius)
-        return true
-      if(source.businessObject.type == ObjectType.InterfaceRequire && target.businessObject.type == ObjectType.InterfaceProvide)
-        return true
+      if (source.businessObject.type == ObjectType.Gropius
+        && target.businessObject.type == ObjectType.Gropius)
+        return true;
+      if (source.businessObject.type == ObjectType.InterfaceRequire
+        && (target.businessObject.type == ObjectType.InterfaceProvide || target.businessObject.type == ObjectType.Gropius))
+        return true;
     }
     return false;
   });
 
-  this.addRule("shape.resize", function (context: any) {
+  this.addRule("shape.resize", function(context: any) {
     const shape = context.shape;
 
     return isFrameElement(shape);
